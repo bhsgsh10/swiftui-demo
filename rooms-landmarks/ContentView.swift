34edbdba70cdc7d58f60b6b9dbefab8eb8d01198
Bhaskar <bhsgsh@gmail.com>
2020-06-15 01:33:55 -0400

    Initial Commit

---
+++ rooms-landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  rooms-landmarks
+//
+//  Created by Bhaskar Ghosh on 6/15/20.
+//  Copyright © 2020 Bhaskar Ghosh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
